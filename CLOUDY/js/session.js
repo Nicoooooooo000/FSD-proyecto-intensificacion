de873e04bb30f53e2b25a57be688eac0cb402c89
Nicoooooooo000 <nicolas9campos@gmail.com>
2025-12-16 19:17:39 -0300

    Add files via upload

---
+++ CLOUDY/js/session.js
@@ -0,0 +1,30 @@
+// ================= REFERENCIAS =================
+const btnLogout = document.getElementById("btnLogout");
+const logoutTitle = document.getElementById("logoutTitle");
+const welcomeMessageSession = document.getElementById("welcomeMessage");
+
+// ================= AL CARGAR LA APP =================
+document.addEventListener("DOMContentLoaded", () => {
+  const session = getSession();
+
+  if (session) {
+    // Si hay sesión activa, ir directo al panel
+    welcomeMessageSession.textContent = `¡Hola ${session.name}! ¡Qué bueno verte!`;
+    showView("panel");
+  } else {
+    // Si no hay sesión, ir al home
+    showView("home");
+  }
+});
+
+// ================= LOGOUT =================
+btnLogout.addEventListener("click", () => {
+  const session = getSession();
+
+  if (session) {
+    logoutTitle.textContent = `¡Hasta luego ${session.name}! ¡Te esperamos!`;
+  }
+
+  clearSession();
+  showView("logout");
+});
